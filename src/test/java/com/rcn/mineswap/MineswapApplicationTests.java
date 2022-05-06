ec15bd98bd80197b3abb925351d73f364dc8db02
Maxim Sergeev <iddqd@Maxims-MacBook-Pro-3.local>
2022-05-06 19:26:14 +0300

    Initial commit

---
+++ src/test/java/com/rcn/mineswap/MineswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcn.mineswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
