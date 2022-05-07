76cdc0b7916a1553705122096ece2e6711c1290c
rcn <raincoven@gmail.com>
2022-05-07 16:49:31 +0300

    Added: refresh game endpoint

--- src/main/java/com/rcn/mineswap/GameController.java
+++ src/main/java/com/rcn/mineswap/GameController.java
@@ -1,14 +1,23 @@
 package com.rcn.mineswap;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
 
     @GetMapping("cellValue/{x}/{y}")
-    String CellValue(int x, int y) {
-        GameInstance instance = GameInstance.getInstance();
-        return String.valueOf(instance.GetCellContent(x,y));
+    String CellValue(@PathVariable Integer x, @PathVariable Integer y) {
+        GameInstance Instance = GameInstance.getInstance();
+        return String.valueOf(Instance.GetCellContent(x,y));
+    }
+
+    @PostMapping("/restart")
+    Boolean Restart() {
+        GameInstance Instance = GameInstance.getInstance();
+        Instance.Restart();
+        return true;
     }
 }
