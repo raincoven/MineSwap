2b37cf74617c979ec4fcec3bd0287429ba499d3b
rcn <raincoven@gmail.com>
2022-05-07 18:28:25 +0300

    Added: js rest calls and cells interaction

---
+++ src/main/resources/static/game.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('div.cell').click(function() {
+        var cell = $(this);
+        var x = cell.data("x");
+        var y = cell.data("y");
+
+        $.ajax({
+            url: "/cellValue/" + x + "/" + y,
+            error: function() {
+                alert("Something went wrong! Reload the page and start an new game.");
+            },
+            success: function(resp) {
+                cell.addClass(resp);
+                cell.removeClass("active");
+                console.log($(this));
+                console.log(resp);
+            }
+        });
+    });
+});
