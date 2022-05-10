2e3ee0ba8410669357c513c9f18b8b4ea7defe47
rcn <raincoven@gmail.com>
2022-05-10 20:39:02 +0300

    Refactored: split huge constructor for several methods

--- src/main/java/com/rcn/mineswap/GameInstance.java
+++ src/main/java/com/rcn/mineswap/GameInstance.java
@@ -9,16 +9,25 @@ public class GameInstance {
     private final Items[][] GameField = new Items[5][5];
 
     public GameInstance() {
-        // Plant the prize
-        int chestXCord = getRandomCord(0, 4);
-        int chestYCord = getRandomCord(0, 4);
-        GameField[chestXCord][chestYCord] = Items.CHEST;
+        int[] prizeCoordinates = plantThePrize();
+        plantTheDynamites(prizeCoordinates[0], prizeCoordinates[1]);
+        plantTheDynamiteMarkers();
+    }
 
-        //Plant the dynamite
+    // Plant the prize
+    private int[] plantThePrize() {
+        int prizeXCoordinate = getRandomCoord(0, 4);
+        int prizeYCoordinate = getRandomCoord(0, 4);
+        GameField[prizeXCoordinate][prizeYCoordinate] = Items.CHEST;
+        return new int[]{prizeXCoordinate, prizeYCoordinate};
+    }
+
+    //Plant the dynamite for cells in range 3 from the point
+    private void plantTheDynamites(int prizeXCoordinate, int prizeYCoordinate) {
         int countDynamite = 0;
         while (countDynamite != 4) {
-            int x = getRandomCord(chestXCord-3, chestXCord+3);
-            int y = getRandomCord(chestYCord-3, chestYCord+3);
+            int x = getRandomCoord(prizeXCoordinate-3, prizeXCoordinate+3);
+            int y = getRandomCoord(prizeYCoordinate-3, prizeYCoordinate+3);
             try {
                 if (GameField[x][y] != Items.CHEST && GameField[x][y] != Items.DYNAMITE) {
                     GameField[x][y] = Items.DYNAMITE;
@@ -26,12 +35,17 @@ public class GameInstance {
                 }
             }
             // Went out of game field boundaries. Just need to get another set of coordinates.
-            catch (Exception e) {
-                continue;
+            catch (Exception ignored) {
             }
         }
+    }
 
-        //Plant EMPTY, COIL and WARN
+    // Plant EMPTY, COIL and WARN to identify if cell is in dynamite boundaries
+    // We are checking only top, left, bottom and right cells
+    // EMPTY - no dynamite nearby
+    // COIL - 1 cell containing dynamite
+    // WARN - more than 1 cell nearby containing the dynamite
+    private void plantTheDynamiteMarkers() {
         for(int x = 0; x < 5; x++) {
             for(int y=0; y < 5; y++) {
                 if (GameField[x][y] == Items.CHEST || GameField[x][y] == Items.DYNAMITE) {
@@ -48,14 +62,15 @@ public class GameInstance {
         }
     }
 
-    private int getRandomCord(int x, int y) {
+
+    private int getRandomCoord(int x, int y) {
         Random random = new Random();
         return random.ints(x, y)
                 .findFirst()
                 .getAsInt();
     }
 
-    // We need to check 4 cells (2 vertical and 2 horizontal)
+    // Checking how many top, bottom, left and right bordering cells contains dynamite
     private int CountDynamiteInNeighborCells(int x, int y) {
         int countDynamite = 0;
 
